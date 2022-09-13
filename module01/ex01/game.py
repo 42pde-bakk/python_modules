a202beef587e87e4a7ce77a8ac73f290e1973da1
pde-bakk <pde-bakk@student.codam.nl>
2022-09-13 18:41:59 +0200

    added docstring to Baratheon class

--- module01/ex01/game.py
+++ module01/ex01/game.py
@@ -5,6 +5,7 @@ class GotCharacter:
 
 
 class Baratheon(GotCharacter):
+    """A class representing the Baratheon family"""
     def __init__(self, first_name: str, is_alive: bool = True):
         super().__init__(first_name, is_alive)
         self.family_name = 'Baratheon'
