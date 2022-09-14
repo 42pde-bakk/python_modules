bc294d6fe5176ef28dcaf0478a8d7efac1efd891
pde-bakk <pde-bakk@student.codam.nl>
2022-09-14 20:09:34 +0200

    whoop:

--- module01/ex01/game.py
+++ module01/ex01/game.py
@@ -1,4 +1,7 @@
 class GotCharacter:
+    """Class representing a fictional character from Game of Thrones
+    (Season 8 was awful)
+    """
     def __init__(self, first_name: str, is_alive: bool = True):
         self.first_name = first_name
         self.is_alive = is_alive
@@ -6,15 +9,14 @@ class GotCharacter:
 
 class Baratheon(GotCharacter):
     """A class representing the Baratheon family"""
-    def __init__(self, first_name: str, is_alive: bool = True):
+    def __init__(self, first_name: str = None, is_alive: bool = True):
+        print('first_name = ', first_name)
         super().__init__(first_name, is_alive)
         self.family_name = 'Baratheon'
         self.house_words = 'Ours is the Fury'
 
-    def print_house_words(self):
+    def print_house_words(self) -> None:
         print(self.house_words)
 
-    def die(self):
-        assert self.is_alive
+    def die(self) -> None:
         self.is_alive = False
-
