0e22aa55769a3364fbeeff43ace097d62f8ed7e8
pde-bakk <pde-bakk@student.codam.nl>
2022-09-26 23:44:52 +0200

    the subject lies: they dont actually want you to drop duplicates, bunch of liars at 42AI

--- module04/ex02/ProportionBySport.py
+++ module04/ex02/ProportionBySport.py
@@ -6,8 +6,8 @@ def proportion_by_sport(df: pd.DataFrame, year: int, sport: str, gender: str):
         return None
     total = df.loc[(df['Sex'] == gender) & (df['Year'] == year)]
     sub = total.loc[df['Sport'] == sport]
-    total = total.drop_duplicates(subset=['Name'])
-    sub = sub.drop_duplicates(subset=['Name'])
+    # total = total.drop_duplicates(subset=['Name'])
+    # sub = sub.drop_duplicates(subset=['Name'])
     if len(sub) == 0:
         return 0.0
     return len(sub) / len(total)
