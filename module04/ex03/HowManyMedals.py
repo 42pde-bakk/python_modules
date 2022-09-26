3ed93312a76fa2b3bcf35420996b201dd5380f3d
pde-bakk <pde-bakk@student.codam.nl>
2022-09-26 23:55:29 +0200

    fixed now also showing years they were a loser

--- module04/ex03/HowManyMedals.py
+++ module04/ex03/HowManyMedals.py
@@ -4,11 +4,13 @@ import pandas as pd
 def how_many_medals(df: pd.DataFrame, name: str) -> dict:
     if not (isinstance(df, pd.DataFrame) and isinstance(name, str)):
         return {}
-    selection = df.loc[(df['Name'] == name) & df['Medal']]
+    selection = df.loc[df['Name'] == name]
     medal_dict = {}
     for index, row in selection.iterrows():
         year = row['Year']
         if year not in medal_dict.keys():
             medal_dict[year] = {'G': 0, 'S': 0, 'B': 0}
-        medal_dict[year][row['Medal'][0]] += 1
+        if not pd.isnull(row['Medal']):
+            # print(row)
+            medal_dict[year][row['Medal'][0]] += 1
     return medal_dict
