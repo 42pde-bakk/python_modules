6a9c0b92d8acb01fa9d33c536a654cdaf62f3a48
pde-bakk <pde-bakk@student.codam.nl>
2022-09-26 23:35:25 +0200

    oops, forgot to account for winter olympics too

--- module04/ex01/YoungestFellah.py
+++ module04/ex01/YoungestFellah.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def youngest_fellah(df: pd.DataFrame, year: int) -> dict[str, float] | None:
-    if not isinstance(df, pd.DataFrame) or not isinstance(year, int) or year % 4 != 0:
+    if not isinstance(df, pd.DataFrame) or not isinstance(year, int):
         print('ERROR')
         return
     men = df.loc[(df['Sex'] == 'M') & (df['Year'] == year)]
