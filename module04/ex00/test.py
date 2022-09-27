e606300973e4d4c18670dd759f1df0b19a73cf59
pde-bakk <pde-bakk@student.codam.nl>
2022-09-27 15:33:15 +0200

    oops, typo

--- module04/ex00/test.py
+++ module04/ex00/test.py
@@ -3,8 +3,9 @@ from FileLoader import FileLoader
 
 def main() -> None:
     loader = FileLoader()
-    data = loader.load("../data/athlete_sevents.csv")
+    data = loader.load("../data/athlete_events.csv")
     loader.display(data, 12)
+    loader.display(data, -12)
 
 
 if __name__ == '__main__':
